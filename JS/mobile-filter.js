08e030c051ae5f6cdc9bc39ff08dcbbe048811e9
Negan-ai <yoosyoos32@gmail.come>
2021-05-30 16:32:27 +0300

    #5

--- JS/mobile-filter.js
+++ JS/mobile-filter.js
@@ -1,18 +1,41 @@
-/* checkbox */
-const checkboxItems = document.querySelectorAll('.checkbox-content__item');
-const checkboxItem = document.querySelector('.checkbox-content__item');
-const checkboxLabeles = document.querySelectorAll('.checkbox-content__label');
-const checkboxLabel = document.querySelector('.checkbox-content__label');
-const inputCheckbox = document.querySelectorAll('checkbox');
-const closeCheckbox = document.querySelector('.checkbox-content__btn');
-
+/* checkbox-filter */
 $(document).ready(function () {
   $('.mobile-category-btn').click(function () {
-    $('.checkbox-content__label').toggleClass('checkbox-content__label--active');
+    $('.checkbox-content__item').toggleClass('checkbox-content__label--active');
     $('.mobile-category-btn').toggleClass('mobile-category-btn--active');
+    $('.checkbox-content__item').removeClass('checkbox-unselected');
   });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.checkbox-content__item').forEach(function (e) {
+    e.addEventListener('click', function (e) {
+      const label = e.currentTarget.dataset.path;
+      document.querySelectorAll('.checkbox-content__item').forEach(function () {
+        document.querySelector(`[data-target='${label}']`).classList.add('checkbox-selected');
+      });
+      const labelStatus = e.currentTarget.dataset.open;
+      document.querySelectorAll('.checkbox-content__item').forEach(function () {
+        document.querySelector(`[data-close='${labelStatus}']`).classList.add('checkbox-selected');
+      });
+    });
+  });
+  document.querySelectorAll('.checkbox-content__btn').forEach(function (e) {
+    e.addEventListener('click', function (e) {
+      const disabledLabel = e.currentTarget.dataset.active;
+
+      document.querySelectorAll('.checkbox-content__item').forEach(function (e) {
+        document.querySelector(`[data-disable='${disabledLabel}']`).removeAttribute('disabled');
+
+        if (e.classList.contains('checkbox-content__label--active')) {
+          document.querySelector(`[data-disable='${disabledLabel}']`).classList.add('checkbox-selected');
+        } else {
+          document.querySelector(`[data-disable='${disabledLabel}']`).classList.add('checkbox-unselected');
+        }
+      });
+    });
+  });
+});
 
 
 
